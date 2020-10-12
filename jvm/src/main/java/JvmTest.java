21c3057c02003ae9d8c1aec2e2a369e57c1c7187
dongxiaosong <dxsjl520@163.com>
2020-10-12 21:07:12 +0800

    '提交'

--- jvm/src/main/java/JvmTest.java
+++ jvm/src/main/java/JvmTest.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class JvmTest {
+    public static void main(String[] args) throws Exception{
+        Runtime runtime=Runtime.getRuntime();
+        System.out.println("初始化堆大小:"+runtime.totalMemory()/1024/1024+" M");
+        System.out.println("空闲堆大小:"+runtime.freeMemory()/1024/1024+" M");
+        System.out.println("最大堆大小:"+runtime.maxMemory()/1024/1024+" M");
+        List result=new ArrayList();
+        while (true){
+            Thread.sleep(5*1000);
+            byte[] data=new byte[1024*1024];
+            result.add(data);
+
+        }
+    }
 }
